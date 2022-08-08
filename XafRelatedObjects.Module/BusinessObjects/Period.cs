05df0d273638770cfcc7ff6392d80f03626c9066
Jose Ojeda <joche.ojeda@bitframeworks.com>
2022-08-08 20:12:57 +0300

    Add project files.

---
+++ XafRelatedObjects.Module/BusinessObjects/Period.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafRelatedObjects.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    [DefaultProperty("Year")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Period : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Period(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+
+        int year;
+
+        public int Year
+        {
+            get => year;
+            set => SetPropertyValue(nameof(Year), ref year, value);
+        }
+        [Association("Period-OneToManys")]
+        public XPCollection<OneToMany> OneToManys
+        {
+            get
+            {
+                return GetCollection<OneToMany>(nameof(OneToManys));
+            }
+        }
+
+    }
+}
---
+++ XafRelatedObjects.Module/Controllers/CustomCloneController.cs
@@ -0,0 +1,79 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using XafRelatedObjects.Module.BusinessObjects;
+
+namespace XafRelatedObjects.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class CustomCloneController : ViewController
+    {
+        SimpleAction ClonePeriod;
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public CustomCloneController()
+        {
+            InitializeComponent();
+            ClonePeriod = new SimpleAction(this, "Clone Period", "View");
+            ClonePeriod.Execute += ClonePeriod_Execute;
+            this.TargetObjectType = typeof(Period);
+
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        private void ClonePeriod_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            var Session = (this.ObjectSpace as XPObjectSpace).Session;
+            CloneHelper cloneHelper = new CloneHelper(Session);
+            Period CurrentPeriod = this.View.CurrentObject as Period;
+            Period NewPeriod = this.ObjectSpace.CreateObject<Period>();
+            NewPeriod.Year = CurrentPeriod.Year + 1;
+            ICollection ReferenceObject = Session.CollectReferencingObjects(CurrentPeriod);
+           
+            foreach (BaseObject item in ReferenceObject)
+            {
+
+                if (ReferenceEquals(item, CurrentPeriod))
+                    continue;
+
+                var NewItem = cloneHelper.Clone(item);
+                var PeriodMembers = NewItem.ClassInfo.PersistentProperties.Cast<XPMemberInfo>().Where(pp => pp.ReferenceType!=null && pp.ReferenceType.ClassType == typeof(Period));
+                foreach (var m in PeriodMembers)
+                {
+                    m.SetValue(NewItem, NewPeriod);
+                }
+            }
+            this.ObjectSpace.CommitChanges();
+            // Execute your business logic (https://docs.devexpress.com/eXpressAppFramework/112737/).
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
